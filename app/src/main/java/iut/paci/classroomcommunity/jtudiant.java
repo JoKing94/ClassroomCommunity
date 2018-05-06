9491e28d63c74952b4faba8a670c166d6e8960db
arond <aron4733@gmail.com>
2018-05-06 12:47:44 +0200

    Maj

---
+++ app/src/main/java/iut/paci/classroomcommunity/jtudiant.java
@@ -0,0 +1,20 @@
+package iut.paci.classroomcommunity;
+
+public class jtudiant {
+
+    private String id_e;
+    private String pseudo;
+    private String status;
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public Boolean getStatus() {
+       return true;
+    }
+
+    public int getId_e() {
+        return Integer.parseInt(this.id_e);
+    }
+}
